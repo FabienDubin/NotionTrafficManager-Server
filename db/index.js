9af6d3a897e48bc16681aa03bdf1e822d2b4def7
Fab Dubin <fabien.dubin@gmail.com>
2025-06-26 11:14:51 +0200

    CI/CD setup

--- db/index.js
+++ db/index.js
@@ -2,17 +2,23 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
 const connectDB = async (uri) => {
+  const dbName = process.env.APP_NAME;
+
+  if (!dbName) {
+    console.error(
+      "❌ ERREUR : la variable APP_NAME est absente du fichier .env"
+    );
+    throw new Error("La variable d'environnement APP_NAME est obligatoire.");
+  }
+
   try {
-    const conn = await mongoose.connect(uri);
+    const conn = await mongoose.connect(uri, { dbName });
     console.log(
-      `Connected to Mongo! Database name: "${conn.connections[0].name}"`
+      `✅ Connected to Mongo! Database name: "${conn.connections[0].name}"`
     );
   } catch (err) {
-    console.error("Error connecting to mongo:", err);
+    console.error("❌ Error connecting to mongo:", err);
     throw err;
   }
 };
